eced9ed4ae88b64a0582e5e006bd431e77486863
Wouter Servaes <wouterservaes@protonmail.com>
2022-01-20 13:41:45 +0100

    Updated project
    Debugging compute shader and project: problem with GetBitPosition()

--- CACloudsProject/Assets/Scripts/CellularAutomatonShader.cs
+++ CACloudsProject/Assets/Scripts/CellularAutomatonShader.cs
@@ -1,6 +1,7 @@
 //These helped:
 //https://catlikecoding.com/unity/tutorials/basics/compute-shaders/
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,6 +17,7 @@ public class CellularAutomatonShader : MonoBehaviour
     private ComputeBuffer _ActNextBuffer, _CldNextBuffer, _HumNextBuffer;
     private ComputeBuffer _CloudPositions;
     private ComputeBuffer _GenCounter;
+    private ComputeBuffer TestBuffer;
     private int TotalInts => _CAGridSettings.TotalCells/32;
     //kernel IDs
     private int _ProcessCellsKernel;
@@ -23,6 +25,8 @@ public class CellularAutomatonShader : MonoBehaviour
     //other IDs
     private int _CloudPositionsId;
     private int _RandomSeedId;
+    private int _GenCounterId;
+    private int _CldBufferId;
 
     //CA Settings
     [SerializeField] private CASettings _CASettings = null;
@@ -55,16 +59,20 @@ public class CellularAutomatonShader : MonoBehaviour
     }
     private void Update()
     {
+        UpdateCAShader();
+        DrawClouds();
+
         if (!_IsPaused)
         {
             _ElapsedSec += Time.deltaTime;
             if (_ElapsedSec >= _CASettings.MinimumUpdateTime)
             {
                 _ElapsedSec = 0f;
-                UpdateCAShader();
+               
+                
             }
         }
-        DrawClouds();
+        
     }
 
     //sets all the variables and buffers of the shader
@@ -72,68 +80,66 @@ public class CellularAutomatonShader : MonoBehaviour
     {
         //initializing buffers
         //CA buffers
-        _ActBuffer = new ComputeBuffer(TotalInts, sizeof(int));
-        _HumBuffer = new ComputeBuffer(TotalInts, sizeof(int));
+        //_ActBuffer = new ComputeBuffer(TotalInts, sizeof(int));
+        //_HumBuffer = new ComputeBuffer(TotalInts, sizeof(int));
         _CldBuffer = new ComputeBuffer(TotalInts, sizeof(int));  
-        _ActNextBuffer = new ComputeBuffer(TotalInts, sizeof(int));
-        _HumNextBuffer = new ComputeBuffer(TotalInts, sizeof(int));
-        _CldNextBuffer = new ComputeBuffer(TotalInts, sizeof(int));  
+        //_ActNextBuffer = new ComputeBuffer(TotalInts, sizeof(int));
+        //_HumNextBuffer = new ComputeBuffer(TotalInts, sizeof(int));
+        //_CldNextBuffer = new ComputeBuffer(TotalInts, sizeof(int));  
 
         //other buffers
         _CloudPositions = new ComputeBuffer(_CAGridSettings.TotalCells, sizeof(float)*3);
         _GenCounter = new ComputeBuffer(1, sizeof(int));
-        
-        //setting buffers
-        //CA buffers
-        _ComputeShader.SetBuffer(_ProcessCellsKernel, "_Act", _ActBuffer);
-        _ComputeShader.SetBuffer(_ProcessCellsKernel, "_Hum", _HumBuffer);
-        _ComputeShader.SetBuffer(_ProcessCellsKernel, "_Cld", _CldBuffer);
-        _ComputeShader.SetBuffer(_ProcessCellsKernel, "_ActNext", _ActNextBuffer);
-        _ComputeShader.SetBuffer(_ProcessCellsKernel, "_HumNext", _HumNextBuffer);
-        _ComputeShader.SetBuffer(_ProcessCellsKernel, "_CldNext", _CldNextBuffer);
-
-        //other buffers
-        _ComputeShader.SetBuffer(_ProcessCellsKernel, "_CloudPositions", _CloudPositions);
-        _ComputeShader.SetBuffer(_ProcessCellsKernel, "_GenCounter", _GenCounter);
-
-        //Settings variables
-        //Grid variables
-        _ComputeShader.SetInt("_CellCount", _CAGridSettings.TotalCells);
-        _ComputeShader.SetInt("_Columns", _CAGridSettings.Columns);
-        _ComputeShader.SetInt("_Rows", _CAGridSettings.Rows);
-        _ComputeShader.SetInt("_Depth", _CAGridSettings.Depth);
-
+        TestBuffer = new ComputeBuffer(_CAGridSettings.TotalCells, sizeof(int));
         //generation variables
-        _ComputeShader.SetInt("_WindStartGen", _CASettings.WindStartGeneration);
-        _ComputeShader.SetInt("_ExtStartGen", _CASettings.ExtStartGeneration);
-        
-        //probability variables
-        _ComputeShader.SetFloat("_PActStart", _CASettings.ActProbabilityAtStart);
-        _ComputeShader.SetFloat("_PHumStart", _CASettings.HumProbabilityAtStart);
-        _ComputeShader.SetFloat("_PExt", _CASettings.ExtProbability);
-        _ComputeShader.SetFloat("_PHum", _CASettings.HumProbability);
-        _ComputeShader.SetFloat("_PAct", _CASettings.ActProbability);
+        //_ComputeShader.SetInt("_WindStartGen", _CASettings.WindStartGeneration);
+        //_ComputeShader.SetInt("_ExtStartGen", _CASettings.ExtStartGeneration);
 
-        
+        //probability variables
+        //_ComputeShader.SetFloat("_PActStart", _CASettings.ActProbabilityAtStart);
+        //_ComputeShader.SetFloat("_PHumStart", _CASettings.HumProbabilityAtStart);
+        //_ComputeShader.SetFloat("_PExt", _CASettings.ExtProbability);
+        //_ComputeShader.SetFloat("_PHum", _CASettings.HumProbability);
+        //_ComputeShader.SetFloat("_PAct", _CASettings.ActProbability);
+
+
         //saving Property IDs
+        //CA buffers
+        _CldBufferId = Shader.PropertyToID("_Cld");
         //kernels
         _ProcessCellsKernel = _ComputeShader.FindKernel("CSProcessCells");
         _InitCellsKernel = _ComputeShader.FindKernel("CSInitializeCells");
         //other
         _CloudPositionsId = Shader.PropertyToID("_CloudPositions");
         _RandomSeedId = Shader.PropertyToID("_RandomSeed");
+        _GenCounterId = Shader.PropertyToID("_GenCounter");
     }
 
     private void UpdateCAShader()
     {
-        _ComputeShader.SetFloat(_RandomSeedId, Time.time);
+        //buffers
         _ComputeShader.SetBuffer(_ProcessCellsKernel, _CloudPositionsId, _CloudPositions);
+        _ComputeShader.SetBuffer(_ProcessCellsKernel, _GenCounterId, _GenCounter);
+        _ComputeShader.SetBuffer(_ProcessCellsKernel, _CldBufferId, _CldBuffer);
+        _ComputeShader.SetBuffer(_ProcessCellsKernel, "TestBuffer", TestBuffer);
+        //variables
+        _ComputeShader.SetInt("_CellCount", _CAGridSettings.TotalCells);
+        _ComputeShader.SetInt("_Columns", _CAGridSettings.Columns);
+        _ComputeShader.SetInt("_Rows", _CAGridSettings.Rows);
+        _ComputeShader.SetInt("_Depth", _CAGridSettings.Depth);
+        _ComputeShader.SetFloat("_CellHeight", _CAGridSettings.CellHeight);
+        _ComputeShader.SetFloat(_RandomSeedId, Time.time);
+       
+
         _ComputeShader.Dispatch(_ProcessCellsKernel, 1,1,1);
 
         //generation counter
         int[] counter = new int[1];
         _GenCounter.GetData(counter);
         Debug.Log(counter[0]);
+
+        int[] test = new int[_CAGridSettings.TotalCells];
+        TestBuffer.GetData(test);
     }
 
     private void DrawClouds()
@@ -144,12 +150,13 @@ public class CellularAutomatonShader : MonoBehaviour
     }
     private void OnDestroy()
     {
-        DestroyBuffer(_ActBuffer);
-        DestroyBuffer(_HumBuffer);
+        //DestroyBuffer(_ActBuffer);
+        //DestroyBuffer(_HumBuffer);
         DestroyBuffer(_CldBuffer);
-        DestroyBuffer(_ActNextBuffer);
-        DestroyBuffer(_HumNextBuffer);
-        DestroyBuffer(_CldNextBuffer);
+        DestroyBuffer(TestBuffer);
+        //DestroyBuffer(_ActNextBuffer);
+        //DestroyBuffer(_HumNextBuffer);
+        //DestroyBuffer(_CldNextBuffer);
         DestroyBuffer(_CloudPositions);
         DestroyBuffer(_GenCounter);
     }
@@ -178,22 +185,19 @@ public class CellularAutomatonShader : MonoBehaviour
         int[] resetArray = new int[TotalInts];
         for (int idx = 0; idx < TotalInts; idx++)
         {
-            resetArray[idx] = 0x000000;
+            resetArray[idx] = 1;
+            //resetArray[idx] = 0x000000;
         }
 
-        _ActBuffer.SetData(resetArray);
-        _HumBuffer.SetData(resetArray);
+        BitArray b = new BitArray(resetArray);
+        bool[] a = new bool[_CAGridSettings.TotalCells];
+        b.CopyTo(a, 0);
+        //_ActBuffer.SetData(resetArray);
+        //_HumBuffer.SetData(resetArray);
         _CldBuffer.SetData(resetArray);
-        _ActNextBuffer.SetData(resetArray);
-        _HumNextBuffer.SetData(resetArray);
-        _CldNextBuffer.SetData(resetArray);
+        //_ActNextBuffer.SetData(resetArray);
+        //_HumNextBuffer.SetData(resetArray);
+        //_CldNextBuffer.SetData(resetArray);
         _GenCounter.SetData(new int[1] { 0 });
-
-       // _ComputeShader.SetBuffer(_InitCellsKernel, "_Act", _ActBuffer);
-       // _ComputeShader.SetBuffer(_InitCellsKernel, "_Hum", _HumBuffer);
-       // _ComputeShader.SetFloat("_RandomSeed", Time.time);
-       // _ComputeShader.Dispatch(_InitCellsKernel, 1,1,1);
-       // _ComputeShader.SetBuffer(_ProcessCellsKernel, "_Act", _ActBuffer);
-       // _ComputeShader.SetBuffer(_ProcessCellsKernel, "_Hum", _HumBuffer);
     }
 }
